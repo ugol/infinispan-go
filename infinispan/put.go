25c24a8b36f5c258d6aaf735ad5af078d930bcee
Ugo Landini <ugo.landini@gmail.com>
2016-03-30 01:35:44 +0200

    added empty DecodePutResponse

--- infinispan/put.go
+++ infinispan/put.go
@@ -12,7 +12,7 @@ func createPut(key []byte, value []byte, messageID uint64, cachename string) []b
 
 	p := NewBuffer([]byte{RequestMagic})
 	p.EncodeVarint(messageID)
-	p.EncodeRawBytes([]byte{ProtocolVersionVersion20, PutRequest})
+	p.EncodeRawBytes([]byte{Protocol20, PutRequest})
 	p.EncodeRawBytes([]byte{0, 0})
 	p.EncodeRawBytes([]byte{ClientIntelligenceBasic})
 	p.EncodeRawBytes([]byte{0})
@@ -21,4 +21,10 @@ func createPut(key []byte, value []byte, messageID uint64, cachename string) []b
 	p.EncodeBytes(value)
 	return p.buf
 
+}
+
+// DecodePutResponse creates a Put Response from a buffer
+func (p *Buffer) DecodePutResponse() (*PutRes, error) {
+	var response = &PutRes{}
+	return response, nil
 }
