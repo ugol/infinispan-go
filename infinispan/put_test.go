2cf009a36ae175ab5df95253c890d45e3e604f0b
Ugo Landini <ugo.landini@gmail.com>
2016-04-08 05:00:32 +0200

    Added put tests with lifespan and maxidle

--- infinispan/put_test.go
+++ infinispan/put_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestCreatePut(t *testing.T) {
+func TestCreateDefaultPut(t *testing.T) {
 
 	expectedPut := []byte{
 		0xA0, //MAGIC BYTE
@@ -18,7 +18,7 @@ func TestCreatePut(t *testing.T) {
 		0x00, //Client Topology ID
 		0x01, //Key length
 		0x32, //Key: 2
-		0x77, //Default Lifespan & Max idle
+		0x77, //Default Lifespan & Maxidle
 		0x04, //Value length
 		0x75, //u
 		0x67, //g
@@ -28,6 +28,100 @@ func TestCreatePut(t *testing.T) {
 
 	MakeID(0)
 	put, _ := createPut([]byte("2"), []byte("ugol"), <-id, "", "0", "0")
+
+	if !bytes.Equal(expectedPut, put) {
+		t.Errorf("Expected %v, was %v", expectedPut, put)
+	}
+
+}
+
+func TestCreatePutWithLifespan(t *testing.T) {
+
+	expectedPut := []byte{
+		0xA0, //MAGIC BYTE
+		0x01, //Message Id
+		0x19, //Protocol version
+		0x01, //Operation
+		0x00, //Cache name length ("" = default)
+		0x00, //Flags
+		0x01, //Client intelligence
+		0x00, //Client Topology ID
+		0x01, //Key length
+		0x32, //Key: 2
+		0x17, //ms Lifespan & Default Maxidle
+		0x05, //Duration in ms
+		0x04, //Value length
+		0x75, //u
+		0x67, //g
+		0x6F, //o
+		0x6c, //l
+	}
+
+	MakeID(0)
+	put, _ := createPut([]byte("2"), []byte("ugol"), <-id, "", "5ms", "0")
+
+	if !bytes.Equal(expectedPut, put) {
+		t.Errorf("Expected %v, was %v", expectedPut, put)
+	}
+
+}
+
+func TestCreatePutWithMaxidle(t *testing.T) {
+
+	expectedPut := []byte{
+		0xA0, //MAGIC BYTE
+		0x00, //Message Id
+		0x19, //Protocol version
+		0x01, //Operation
+		0x00, //Cache name length ("" = default)
+		0x00, //Flags
+		0x01, //Client intelligence
+		0x00, //Client Topology ID
+		0x01, //Key length
+		0x32, //Key: 2
+		0x71, //Default Lifespan & ms Maxidle
+		0x05, //Maxidle in ms
+		0x04, //Value length
+		0x75, //u
+		0x67, //g
+		0x6F, //o
+		0x6c, //l
+	}
+
+	MakeID(0)
+	put, _ := createPut([]byte("2"), []byte("ugol"), <-id, "", "0", "5ms")
+
+	if !bytes.Equal(expectedPut, put) {
+		t.Errorf("Expected %v, was %v", expectedPut, put)
+	}
+
+}
+
+func TestCreatePutWithLifespanAndMaxidle(t *testing.T) {
+
+	expectedPut := []byte{
+		0xA0, //MAGIC BYTE
+		0x00, //Message Id
+		0x19, //Protocol version
+		0x01, //Operation
+		0x00, //Cache name length ("" = default)
+		0x00, //Flags
+		0x01, //Client intelligence
+		0x00, //Client Topology ID
+		0x01, //Key length
+		0x32, //Key: 2
+		0x12, //ms Lifespan & ns Maxidle
+		0x05, //Lifespan in ms
+		0x04, //Maxidle in ms
+		0x04, //Value length
+		0x75, //u
+		0x67, //g
+		0x6F, //o
+		0x6c, //l
+	}
+
+	MakeID(0)
+	put, _ := createPut([]byte("2"), []byte("ugol"), <-id, "", "5ms", "4ns")
 
 	if !bytes.Equal(expectedPut, put) {
 		t.Errorf("Expected %v, was %v", expectedPut, put)
