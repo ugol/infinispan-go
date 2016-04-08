bf1c9b28c29fc7c88b7683d913938ede056225de
Ugo Landini <ugo.landini@gmail.com>
2016-04-08 02:59:34 +0200

    Added duration tests table

--- infinispan/duration_test.go
+++ infinispan/duration_test.go
@@ -1,32 +1,56 @@
 package infinispan
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
-func TestDurationInMs(t *testing.T) {
-	duration := "5000 ms"
-	d, time, err := parseDuration(duration)
+func TestParsingDuration(t *testing.T) {
 
-	if err != nil {
-		t.Errorf("Shouldn't generate an error, but it generated '%s'", err)
+	var tests = []struct {
+		input    string
+		duration int
+		time     byte
+		err      error
+	}{
+		{"5000 ms", 5000, 1, nil},
+		{"5000ms", 5000, 1, nil},
+		{"20s", 20, 0, nil},
+		{"100 ns", 100, 2, nil},
+		{"100000μs", 100000, 3, nil},
+		{"2m", 2, 4, nil},
+		{"1h", 1, 5, nil},
+		{"1d", 1, 6, nil},
+		{"-125", -1, 8, nil},
+		{"0", 0, 7, nil},
+		{"5000", 0, 7, fmt.Errorf("Positive duration 5000 provided without time unit")},
+		{"1mm", 0, 7, fmt.Errorf("Unknown duration format for 1mm")},
+		{"1hh", 0, 7, fmt.Errorf("Unknown duration format for 1hh")},
+		{"1dd", 0, 7, fmt.Errorf("Unknown duration format for 1dd")},
+		{"1dd", 0, 7, fmt.Errorf("Unknown duration format for 1dd")},
+		{"1hd", 0, 7, fmt.Errorf("Unknown duration format for 1hd")},
+		{"1 sm", 0, 7, fmt.Errorf("Unknown duration format for 1 sm")},
+		{"1sm", 0, 7, fmt.Errorf("Unknown duration format for 1sm")},
+		{"1n", 0, 7, fmt.Errorf("Unknown duration format for 1n")},
+		{"-1s", 0, 7, fmt.Errorf("Unknown duration format for -1s")},
 	}
 
-	if d != 5000 {
-		t.Errorf("Expected %d, was %d", 5000, d)
-	}
+	for _, test := range tests {
+		d, time, err := parseDuration(test.input)
 
-	if time != 2 {
-		t.Errorf("'ms' should be encoded with byte %d, but was %d", 1, time)
-	}
-}
-
-func TestSplit(t *testing.T) {
-
-	/*
-		splitter = regexp.MustCompile(`^(\d+)\s{0,}(s|ms|ns|μs|m|h|d)\s{0,}$`)
-		result2 := splitter.FindStringSubmatch("5000ms")
-		for k, v := range result2 {
-			fmt.Printf("%d. %s\n", k, v)
+		if !reflect.DeepEqual(err, test.err) {
+			t.Errorf("Error should be '%v', but it was '%v'", test.err, err)
 		}
-	*/
+
+		if d != test.duration {
+			t.Errorf("Expected %d, was %d", test.duration, d)
+		}
+
+		if time != test.time {
+			t.Errorf("With '%s' expected duration in byte is %d, but was %d", test.input, test.time, time)
+		}
+
+	}
 
 }
